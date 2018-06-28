9f9359b41b17715ad00204e9591c675471e0cdca
Austin Gatto <Austin Gatto@DESKTOP-QQT0EDN>
2018-06-29 06:03:23 +1000

    linear searches done

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,22 +13,49 @@ namespace ConsoleApp1
         {
             List<int> numbers = csvReader.ReadCSV(@"F:\Visual Studio Solutions\AlgorithmsSort-Search\unsorted_numbers.csv");
 
-            DateTime preSort;
-            DateTime postSort;
+            DateTime pre;
+            DateTime post;
 
-            preSort = System.DateTime.Now;
+            pre = System.DateTime.Now;
             List<int> insertionSorted = Insertion.Sort(numbers);
-            postSort = System.DateTime.Now;
-            TimeSpan insertionTime = postSort - preSort;
+            post = System.DateTime.Now;
+            TimeSpan insertionTime = post - pre;
 
-            preSort = System.DateTime.Now;
+            pre = System.DateTime.Now;
             List<int> shellSorted = Shell.Sort(numbers);
-            postSort = System.DateTime.Now;
-            TimeSpan shellTime = postSort - preSort;
+            post = System.DateTime.Now;
+            TimeSpan shellTime = post - pre;
 
-            Console.WriteLine("Time in milliseconds:");
+            Console.WriteLine("Sort time in milliseconds:");
             Console.WriteLine($"insertionSorted: { insertionTime.TotalMilliseconds}");
             Console.WriteLine($"shellSorted: { shellTime.TotalMilliseconds}");
+
+            int[] numsToSearch = new int[]
+            {
+                insertionSorted[0],
+                insertionSorted[1500],
+                insertionSorted[3000],
+                insertionSorted[4500],
+                insertionSorted[6000],
+                insertionSorted[7500],
+                insertionSorted[9000],
+                insertionSorted[10500],
+                insertionSorted[12000],
+                insertionSorted[13500],
+                insertionSorted.Last(),
+            };
+
+            Console.WriteLine("\nLinear Searching:");
+            pre = System.DateTime.Now;
+            foreach (int num in numsToSearch)
+            {
+                int? index = Linear.Search(insertionSorted, num);
+                Console.WriteLine($"num {num} found at index {index}");
+            }
+            post = System.DateTime.Now;
+            TimeSpan linearTime = post - pre;
+            Console.WriteLine($"Linear search time(milliseconds): {linearTime.TotalMilliseconds}");
+
 
             Console.ReadKey();
         }
---
+++ Sort-Search_Library/Linear.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_Search_Library
+{
+    public static class Linear
+    {
+        public static int? Search(List<int> list, int item)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] == item)
+                {
+                    return i;
+                }
+            }
+            return null;
+        }
+    }
+}
