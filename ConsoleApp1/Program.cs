5dcc97875b839313529341fdbe9367a70e00da43
in de <asvosetdph@gmail.com>
2018-07-01 12:24:51 +1000

    complete

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,11 +16,13 @@ namespace ConsoleApp1
             DateTime pre;
             DateTime post;
 
+            //INSERTION SORT
             pre = System.DateTime.Now;
             List<int> insertionSorted = Insertion.Sort(numbers);
             post = System.DateTime.Now;
             TimeSpan insertionTime = post - pre;
 
+            //SHELL SORT
             pre = System.DateTime.Now;
             List<int> shellSorted = Shell.Sort(numbers);
             post = System.DateTime.Now;
@@ -44,20 +46,95 @@ namespace ConsoleApp1
                 insertionSorted[13500],
                 insertionSorted.Last(),
             };
+            
 
-            Console.WriteLine("\nLinear Searching:");
+            //LINEAR SEACRHING(sorted)
+            Console.WriteLine("\nLinear Searching(sorted):");
             pre = System.DateTime.Now;
             foreach (int num in numsToSearch)
             {
-                int? index = Linear.Search(insertionSorted, num);
-                Console.WriteLine($"num {num} found at index {index}");
+                int? LinearIndex = Linear.Search(insertionSorted, num);
+                Console.WriteLine($"num {num} found at index {LinearIndex}");
             }
             post = System.DateTime.Now;
-            TimeSpan linearTime = post - pre;
-            Console.WriteLine($"Linear search time(milliseconds): {linearTime.TotalMilliseconds}");
+            TimeSpan linearSortedTime = post - pre;
+            Console.WriteLine($"Linear search time(milliseconds): {linearSortedTime.TotalMilliseconds}");
 
+            //LINEAR SEACRHING(unsorted)
+            Console.WriteLine("\nLinear Searching(unsorted):");
+            pre = System.DateTime.Now;
+            foreach (int num in numsToSearch)
+            {
+                int? LinearIndex = Linear.Search(numbers, num);
+                Console.WriteLine($"num {num} found at index {LinearIndex}");
+            }
+            post = System.DateTime.Now;
+            TimeSpan linearUnsortedTime = post - pre;
+            Console.WriteLine($"Linear search time(milliseconds): {linearUnsortedTime.TotalMilliseconds}");
 
-            Console.ReadKey();
+            //BINARY SEARCH
+            Console.WriteLine("\nBinary Searching:");
+            pre = System.DateTime.Now;
+            foreach (int num in numsToSearch)
+            {
+                int? BinaryIndex = Binary.Search(insertionSorted, num);
+                Console.WriteLine($"num {num} found at index {BinaryIndex}");
+            }
+            post = System.DateTime.Now;
+            TimeSpan binaryTime = post - pre;
+            Console.WriteLine($"Binary search time(milliseconds): {binaryTime.TotalMilliseconds}");
+
+            //SEARCHING A LIST OF 1 to 10,000,000
+            List<int> TestList = Enumerable.Range(0, 10000000).ToList();
+
+            numsToSearch = new int[]
+            {
+                insertionSorted[0],
+                insertionSorted[1500],
+                insertionSorted[3000],
+                insertionSorted[4500],
+                insertionSorted[6000],
+                insertionSorted[7500],
+                insertionSorted[9000],
+                insertionSorted[10500],
+                insertionSorted[12000],
+                insertionSorted[13500],
+                insertionSorted.Last(),
+                TestList.Last(),
+                9999283,
+                12345678,
+                10,
+                0,
+                568
+            };
+
+            Console.WriteLine("\nSEARCHING A LIST OF 1 to 10,000,000");
+
+            //LINEAR SEACRHING(sorted)
+            Console.WriteLine("\nLinear Searching(sorted):");
+            pre = System.DateTime.Now;
+            foreach (int num in numsToSearch)
+            {
+                int? LinearIndex = Linear.Search(TestList, num);
+                Console.WriteLine($"num {num} found at index {LinearIndex}");
+            }
+            post = System.DateTime.Now;
+            linearSortedTime = post - pre;
+            Console.WriteLine($"Linear search time(milliseconds): {linearSortedTime.TotalMilliseconds}");
+
+            //BINARY SEARCH
+            Console.WriteLine("\nBinary Searching:");
+            pre = System.DateTime.Now;
+            foreach (int num in numsToSearch)
+            {
+                int? BinaryIndex = Binary.Search(TestList, num);
+                Console.WriteLine($"num {num} found at index {BinaryIndex}");
+            }
+            post = System.DateTime.Now;
+            binaryTime = post - pre;
+            Console.WriteLine($"Binary search time(milliseconds): {binaryTime.TotalMilliseconds}");
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Sort-Search_Library/Binary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_Search_Library
+{
+    public class Binary
+    {
+        public static int? Search(List<int> list, int item)
+        {
+            //Algorithm sourced from: https://rosettacode.org/wiki/Binary_search
+
+            int low = 0;
+            int mid;
+            int high = list.Count - 1;
+            while (low <= high)
+            {
+                mid = (low + high) / 2;
+                if (list[mid] > item)
+                {
+                    high = mid - 1;
+                }
+                else if (list[mid] < item)
+                {
+                    low = mid + 1;
+                }
+                else if (list[mid] == item)
+                {
+                    return mid;
+                }
+            }
+            return null;
+        }
+    }
+}
